90b3f107e2a6fe0c46dd1b5723a055a1960c224b
siegfriedbz <siegfried.bozza@yahoo.com>
2023-09-17 10:03:25 +0200

    fix images

--- src/data/images/teachAllImg.js
+++ src/data/images/teachAllImg.js
@@ -1,19 +1,19 @@
 import teach01 from '../../../public/images/teach/teach_01.jpg'
-// import teach02 from '../../../public/images/teach/teach_02.jpg'
+import teach02 from '../../../public/images/teach/teach_02.jpg'
 import teach03 from '../../../public/images/teach/teach_03.jpg'
 import teach04 from '../../../public/images/teach/teach_04.jpg'
 import teach05 from '../../../public/images/teach/teach_05.jpg'
-// import teach06 from '../../../public/images/teach/teach_06.jpg'
+import teach06 from '../../../public/images/teach/teach_06.jpg'
 import teach07 from '../../../public/images/teach/teach_07.jpg'
 import teach08 from '../../../public/images/teach/teach_08.jpg'
 
 const teachAllImg = {
   teach01,
-  // teach02,
+  teach02,
   teach03,
   teach04,
   teach05,
-  // teach06,
+  teach06,
   teach07,
   teach08,
 }
