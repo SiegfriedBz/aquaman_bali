17f9b92143215933529af35bfe1e325b97ae382d
siegfriedbz <siegfried.bozza@yahoo.com>
2023-09-17 09:54:04 +0200

    fix images

--- src/data/images/teachAllImg.js
+++ src/data/images/teachAllImg.js
@@ -1,9 +1,9 @@
 import teach01 from '../../../public/images/teach/teach_01.jpg'
-import teach02 from '../../../public/images/teach/teach_02.jpg'
+// import teach02 from '../../../public/images/teach/teach_02.jpg'
 import teach03 from '../../../public/images/teach/teach_03.jpg'
 import teach04 from '../../../public/images/teach/teach_04.jpg'
 import teach05 from '../../../public/images/teach/teach_05.jpg'
-import teach06 from '../../../public/images/teach/teach_06.jpg'
+// import teach06 from '../../../public/images/teach/teach_06.jpg'
 import teach07 from '../../../public/images/teach/teach_07.jpg'
 import teach08 from '../../../public/images/teach/teach_08.jpg'
 
